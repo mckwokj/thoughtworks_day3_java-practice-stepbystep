6be0dfe6d887ecb5a4819bb7cad2e72d2538d2fc
Johnson Kwok <kwokjo2@oocl.com>
2021-12-01 17:35:03 +0800

    [OOPStepByStep] [Johnson Kwok] feat: complete practice05

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,24 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return klass != 0 ? super.introduce() + " I am a Teacher. I teach Class " + klass + "."
+                : super.introduce() + " I am a Teacher. I teach No Class.";
+    }
 }
