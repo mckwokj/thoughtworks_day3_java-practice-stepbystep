d3ada1e209137b99c690354273e4433fc6019f2f
Johnson Kwok <kwokjo2@oocl.com>
2021-12-01 18:37:07 +0800

    [OOPStepByStep] [Johnson Kwok] feat: complete practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,46 @@
 package practice08;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> members = new ArrayList<>();
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        String output = "It is not one of us.";
+        for (Student member: members) {
+            System.out.println();
+            if (member.equals(student)) {
+                leader = student;
+                output = "";
+                break;
+            }
+        }
+
+        if (!output.equals("")) {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student) {
+        members.add(student);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
 }
